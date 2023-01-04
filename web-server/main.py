cd7d860a25c589a1c49d0bc5599e8adbd2994807
Luis Marquez <luisjose0527@gmail.com>
2023-01-04 17:56:07 -0400

    Using FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Esta es una pagina enviada desde python</h1>
+        <p>Como avanza la tecnologia ah, que loco</p>
+    """
+
 
 def run():
     store.get_categories()
